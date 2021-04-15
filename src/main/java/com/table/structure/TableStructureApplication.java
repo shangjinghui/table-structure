603c2ffe142bcb472179fe29dee736380524dab0
shangjinghui <shangjinghui@egova.com.cn>
2021-04-15 21:04:27 +0800

    [A]创建

---
+++ src/main/java/com/table/structure/TableStructureApplication.java
@@ -0,0 +1,13 @@
+package com.table.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableStructureApplication.class, args);
+    }
+
+}
